c4069a50ec3f2ad81bfda20eb177cfe725c03c91
kanotan <kanon.ayuayu@gmail.com>
2020-05-11 07:25:50 +0900

    Tagモデルを作成

---
+++ app/models/tag.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  name_en    :string
+#  name_kana  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tags_on_name  (name) UNIQUE
+#
+class Tag < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_112557) do
+ActiveRecord::Schema.define(version: 2020_04_04_205642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_kana"
+    t.string "name_en"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
